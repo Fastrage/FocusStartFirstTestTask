5a9f1ab509b3c7b8b8f345df5c0bd67136d32d1f
Oleg Krylov <Fastrage@mail.ru>
2019-07-11 15:08:39 +0700

    Initial Commit

---
+++ FocusStartFirstTestTask/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  FocusStartFirstTestTask
+//
+//  Created by Олег Крылов on 11/07/2019.
+//  Copyright © 2019 OlegKrylov. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
